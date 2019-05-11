46606721c1b90b90474785ffb38c1c49f7452333
Your Name <you@example.com>
2019-05-11 21:50:07 +0200

    AnimalFarm exercise - playing animal sound that user demands

--- src/exercises/AnimalFarm.java
+++ src/exercises/AnimalFarm.java
@@ -10,9 +10,34 @@ import javax.swing.JOptionPane;
 public class AnimalFarm {
 
 	AnimalFarm() {
-		/* 1. Ask the user which animal they want, then play the sound of that animal. */
 
+		/*
+		 * 1. Ask the user which animal they want, then play the sound of that animal.
+		 */
 		/* 2. Make it so that the user can keep entering new animals. */
+
+		for (;;) {
+
+			String animal = JOptionPane
+					.showInputDialog("What animal sound do you want to hear?\n Cow? Duck? Dog, cat or llama?");
+
+			if (animal.equals("cow")) {
+				playMoo();
+			} else if (animal.equals("duck")) {
+				playQuack();
+			} else if (animal.equals("dog")) {
+				playWoof();
+			} else if (animal.equals("cat")) {
+				playMeow();
+			} else if (animal.equals("llama")) {
+				playLlama();
+			} else if (animal.equals("")) {
+				break;
+			} else {
+				JOptionPane.showMessageDialog(null, "Please enter some of the suggested animals.");
+			}
+
+		}
 
 	}
 
@@ -26,6 +51,14 @@ public class AnimalFarm {
 
 	void playWoof() {
 		playNoise(woofFile);
+	}
+
+	void playMeow() {
+		playNoise(meowFile);
+	}
+
+	void playLlama() {
+		playNoise(llamaFile);
 	}
 
 	String quackFile = "sound/quack.wav";
