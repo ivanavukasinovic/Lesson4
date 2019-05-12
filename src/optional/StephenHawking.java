21e2a2767e47c093dbf677ac41c44a8d87145c61
Your Name <you@example.com>
2019-05-12 16:08:22 +0200

    StephenHawking - using speak method

--- src/optional/StephenHawking.java
+++ src/optional/StephenHawking.java
@@ -1,15 +1,29 @@
 package optional;
 
+import javax.swing.JOptionPane;
+
 import voce.SpeechSynthesizer;
 
 public class StephenHawking {
 
 	// 1. make a main method and put steps 2, 3 & 4 inside it
-	// 2. ask the user for a sentence
-	// 3. call the speak method below and send it the sentence
-	// 4. repeat steps 2 and 3 a lot of times
+	public static void main(String[] args) {
 
-	
+		// 4. repeat steps 2 and 3 a lot of times
+		for (int i = 0; i < 10; i++) {
+
+			// 2. ask the user for a sentence
+			String answer = JOptionPane.showInputDialog("Enter a sentence:");
+
+			// 3. call the speak method below and send it the sentence
+			if (answer.equals("")) {
+				break;
+			} else {
+				speak(answer);
+			}
+		}
+	}
+
 	/* Don’t change this…. */
 	static void speak(String words) {
 		SpeechSynthesizer speaker = new SpeechSynthesizer("speaker");
