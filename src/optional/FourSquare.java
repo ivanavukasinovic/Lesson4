60d87a29ae31ff7a4da7246bd4adc5554e0078fc
Your Name <you@example.com>
2019-05-12 16:05:16 +0200

    FourSquare exercise - drawing four squares

--- src/optional/FourSquare.java
+++ src/optional/FourSquare.java
@@ -2,32 +2,45 @@ package optional;
 
 import javax.swing.JOptionPane;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class FourSquare {
-	
+
 	// 2. Create a new Robot
-	
+	Robot robot = new Robot("mini");
 
 	void go() {
+
 		// 4. Make the robot move as fast as possible
+		robot.setSpeed(10);
 
 		// 5. Set the pen width to 5
+		robot.setPenWidth(5);
 
 		// 6. Do steps #7 to #8 four times...
+		for (int i = 0; i < 4; i++) {
 
 			// 7. Set the pen color to random
-	
+			robot.setRandomPenColor();
+
 			// 1. Call the drawSquare() method
-	
+			drawSquare();
+
 			// 8. Turn the robot 90 degrees to the right
+			robot.turn(90);
+		}
 
 	}
 
 	/* 3. Fill in the code to draw a square inside the method below. */
 	void drawSquare() {
 		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
+		for (int i = 0; i < 4; i++) {
+			robot.penDown();
+			robot.move(100);
+			robot.turn(90);
+		}
 
-		
-		
 	}
 
 	public static void main(String[] args) {
